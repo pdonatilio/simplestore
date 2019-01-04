b1b8351c5fa266647a3bfb2ebfaa91cc66fbc454
Paulo Donatilio <pdonatilio@gmail.com>
2019-01-04 11:19:24 +0000

    First Commit - Starting a New Project

---
+++ src/main/java/com/pdonatilio/simplestore/SimplestoreApplication.java
@@ -0,0 +1,14 @@
+package com.pdonatilio.simplestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplestoreApplication.class, args);
+	}
+
+}
+
