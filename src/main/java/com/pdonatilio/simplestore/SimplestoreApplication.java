6d7c33483b5c96f9e46409f3134e1553fb9d7c85
Paulo Donatilio <pdonatilio@gmail.com>
2019-01-04 12:07:41 +0000

    Including Category and Product classes

--- src/main/java/com/pdonatilio/simplestore/SimplestoreApplication.java
+++ src/main/java/com/pdonatilio/simplestore/SimplestoreApplication.java
@@ -1,20 +1,48 @@
 package com.pdonatilio.simplestore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.pdonatilio.simplestore.domain.Category;
+import com.pdonatilio.simplestore.domain.Product;
+import com.pdonatilio.simplestore.repositories.CategoryRepository;
+import com.pdonatilio.simplestore.repositories.ProductRepository;
+
 @SpringBootApplication
 public class SimplestoreApplication implements CommandLineRunner {
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+	@Autowired
+	private ProductRepository productRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SimplestoreApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
 		
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		Product p1 = new Product(null, "Computador", 2000.00);
+		Product p2 = new Product(null, "Impressora", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+		
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
