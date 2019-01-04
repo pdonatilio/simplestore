072979b1727e93cf85b71f0c47cf0fabf90af8cc
Paulo Donatilio <pdonatilio@gmail.com>
2019-01-04 11:25:59 +0000

    Preparing the enviroment to use JPA and h2 database

--- src/main/java/com/pdonatilio/simplestore/SimplestoreApplication.java
+++ src/main/java/com/pdonatilio/simplestore/SimplestoreApplication.java
@@ -1,13 +1,20 @@
 package com.pdonatilio.simplestore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimplestoreApplication {
+public class SimplestoreApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimplestoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
